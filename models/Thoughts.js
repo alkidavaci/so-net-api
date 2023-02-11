1bb34d45ed4b6fb591e3e8b37defeab4a80463bd
Alkida Vaci <alkida.vaci@gmail.com>
2023-02-11 14:45:26 -0500

    Added thoughtSchema

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -30,3 +30,46 @@ const ReactionsSchema = new Schema(
     }
 );
 
+// Schema to create thoughtSchema model
+const thoughtSchema = new Schema (
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY"),
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+             // Allow to use virtuals and getters
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+)
+
+
+// Create a virtual property "reactionCount" that get total count of reactions
+thoughtSchema.virtual('reactionCount')
+.get(function() {
+    return this.reactions.length;
+})
+
+// Initialize the Thought model
+const Thought = model('Thought', thoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
+
