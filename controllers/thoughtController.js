f01965639138d8dee2d53d12cd3fb8eba1d09032
Alkida Vaci <alkida.vaci@gmail.com>
2023-02-13 11:11:35 -0500

    Controller create a thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,26 +1,49 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-
   // Get all thoughts. Route '/'
   getAllThought(req, res) {
     // Receive all the documents from Thought
     Thought.find({})
       // Return data in json format
-      .then((thought) => res.json(thought))
+      .then((thoughtData) => res.json(thoughtData))
       .catch((err) => res.status(500).json(err));
   },
-  // Get one thought. Route '/'
+
+  // Create one thought. Route '/'
+  createThought(req, res) {
+    // Create a new thought in the "Thought" collection
+    Thought.create(req.body)
+      .then(({ _id }) => {
+        // Find a user with and push a thought
+        return User.findOneAndUpdate(
+          // Filter the query by requested id
+          { _id: req.body.userId },
+          // Push the new thought ID to the "thoughts" array
+          { $push: { thoughts: _id } },
+          // Return the updated document
+          { new: true }
+        );
+      })
+      .then((thoughtData) =>
+        !thoughtData
+          ? res.status(404).json({ message: "No User find with this ID!" })
+          : res.json(thoughtData)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Get one thought. Route '/:thoughtId'
   getOneThought(req, res) {
     // Retrieve a single thought from the "Thought" collection
     Thought.findOne({ _id: req.params.thoughtId })
       // Exclude the "__v" property from the returned user document
       .select("-__v")
-      .then((thought) =>
-        !thought
-          ? res.status(404).json({ message: "No Thought find with this ID!" })
-          : res.json(thought)
+      .then((thoughtData) =>
+        !thoughtData
+          ? res.status(404).json({ message: "No thought with this ID!" })
+          : res.json(thoughtData)
       )
       .catch((err) => res.status(500).json(err));
-  }
+  },
 };
