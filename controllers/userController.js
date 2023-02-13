e5a4e4b83e7826c9f34ad2356d09bf4a908269fa
Alkida Vaci <alkida.vaci@gmail.com>
2023-02-12 20:29:33 -0500

    Functions to get a single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const { User, Thought } = require("../models");
 
 
 module.exports = {
-  
+
     // Get all users. Route '/'
     getAllUser(req, res) {
       // Receive all the documents from Users
@@ -20,7 +20,25 @@ module.exports = {
           // Return data in json format
         .then((user) => res.json(user))
         .catch((err) => res.status(500).json(err));
-    }
+    },
+
+    // Get single user.  Route '/:userId'
+  getSingleUser(req, res) {
+    // Retrieve a single user from the "User" collection
+    User.findOne({ _id: req.params.userId })
+      // Methods to populate "thought" documents
+      .populate("thoughts")
+      // Methods to populate "friend" documents
+      .populate("friends")
+      // Exclude the "__v" property from the returned user document
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User with that ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  }
 
   };
    
